4e954c086db38d5e70e7dad635b864e182854001
Guang Yang <57535739+xhiifire@users.noreply.github.com>
2020-11-08 15:15:30 +0000

    added mongoDB Loader
    mongoDB loader is ready, however the csv to mongo initialization file is not ready

--- dataloader.py
+++ dataloader.py
@@ -120,7 +120,7 @@ class Data_Loader_CSV(Data_Loader):
                 try:
                     row = df.loc[ticker]
                 except:
-                    pass  # Pass for now, implement method for checking missing data later
+                    pass  #TODO: Pass for now, implement method for checking missing data later
                 df_row = row.to_frame().transpose()
                 df_row["datetime"] = date_time
                 df_row = df_row.set_index("datetime")
@@ -132,7 +132,7 @@ class Data_Loader_CSV(Data_Loader):
                     )
                 except KeyError:
                     data_dict[ticker] = df_row
-        # A mapping between stock ticker and price data needs to be there
+        #TODO: A mapping between stock ticker and price data needs to be there
         return data_dict
 
     def return_features(self) -> typing.List[str]:
@@ -145,10 +145,11 @@ class Data_Loader_CSV(Data_Loader):
     def _extract_filesnames_from_date(self) -> typing.List[str]:
         """
         :return: List of filenames from start date to end date
+        :raise DateNoInvalidException if date does not exist in dataset
         """
         dt = sorted(list(self.__datetime_filename_HashMap))
 
-        # How to handle weekend and trading holidays?
+        #TODO: How to handle weekend and trading holidays?
         try:
             start_index = dt.index(self.start)
         except:
@@ -223,8 +224,126 @@ class Data_Loader_CSV(Data_Loader):
 # MongoDB Data Loader
 # =============================================================================
 
-# class Data_Loader_mongo(Data_Loader):
-#     pass
+class Data_Loader_mongo(Data_Loader):
+    """
+    Data Loader from mongoDB
+    """
+    
+    def __init__(self,datasource: str, 
+                  tickers: typing.List[str],
+                  features: typing.List[str],
+                  start: datetime,
+                  end: datetime
+                  ):
+        """
+         :param datasource: name of database in mongoDB
+         :param tickers: symbols/tickers of the stocks you want to load
+         :param features: features you want to extract
+         :param start: start date
+         :param end: end date ( result includes ending date)
+        """
+        super().__init__(datasource, tickers, features, start, end)
+        client = MongoClient()
+        self._db = client[datasource]
+        if len(self._db.list_collection_names()) == 0:
+            raise EmptyDatabase(f"{datasource} is an empty database")
+        self._features_list = self.return_features()
+    
+    
+    def load_data(self) -> typing.Dict[str, pd.DataFrame]:
+        """
+        :return: List of Dataframes (each df represents the time series for a particular stock)
+        :raise FeaturesMismatchException if feature does not exist in dataset
+        """
+        if len(self.features) == 0:
+            columns = self._features_list
+        else:
+            if len(set(self.features).intersection(set(self._features_list))) == len(self.features):
+                features = set(self.features)
+                features.add("symbol")
+                features.add("datetime")
+                columns = list(features)
+            else:
+                raise Exception("FeaturesMismatchException: Some input features not present in dataset")
+        
+        columns_dict= {column:1 for column in columns}
+        columns_dict["_id"] = 0   
+        print(columns_dict)
+        
+        data_dict={}
+        for ticker in self.tickers:
+            collection = self._db[ticker]
+            if collection.count_documents({}) == 0:
+                raise Exception(f"{ticker} collection is empty (check ticker name)")
+        
+            range_query_statement = {'datetime':{"$gte":self.start,"$lte":self.end}}
+            query_result = pd.DataFrame(collection.find(range_query_statement,columns_dict).sort("datetime"))
+            query_result = query_result.set_index("datetime")
+            
+            try:
+                data_dict[ticker] = data_dict[ticker].append(query_result, verify_integrity=True)
+            except KeyError:
+                data_dict[ticker] = query_result
+        
+        return data_dict
+        
+    def return_features(self) -> typing.List[str]:
+        """
+        :return: List of features found in dataset (column names)
+        """
+        collection = self._db[self._db.list_collection_names()[0]]
+        features = list(collection.find_one())
+        features.remove("_id")
+        return features
+    
+    def compute_features(
+        self, features: typing.List[str]
+    ) -> typing.Dict[str, pd.DataFrame]:
+        raw_data_dict = self.load_data()
+        data_dict = dict()
+        feature_map = {
+            "return": np.sum,
+            "volatility": np.std,
+            "skewness": scipy.stats.skew,
+            "kurtosis": scipy.stats.kurtosis,
+        }
+
+        def compute_split_ratio(entry):
+            if not np.isnan(entry):
+                previous_ratio = np.float(entry.split(":")[0])
+                after_ratio = np.float(entry.split(":")[1])
+                return previous_ratio / after_ratio
+            else:
+                return 1.0
+
+        for ticker, raw_df in raw_data_dict.items():
+            # compute adjusted_close with roll forward method,
+            # which add the dividend back to the price
+            raw_df["div"] = raw_df["div"].fillna(0)
+            raw_df["adjust_cum"] = (
+                raw_df["adjustment"].apply(compute_split_ratio).cumprod()
+            )
+            raw_df["adjust_div"] = raw_df["div"] * raw_df["adjust_cum"]
+            raw_df["adjust_close"] = (
+                raw_df["close"] * raw_df["adjust_cum"] + raw_df["adjust_div"].cumsum()
+            )
+            # compute t-cost and return
+            raw_df["return"] = raw_df["adjust_close"].apply(lambda x: np.log(x)).diff(1)
+            raw_df["tcost"] = (raw_df["ask"] - raw_df["bid"]) / (
+                raw_df["ask"] + raw_df["bid"]
+            )
+
+            for f in features:
+                f_funcstr = f.split("_")[0]
+                f_lookback = np.int(f.split("_")[1])
+                raw_df[f] = (
+                    raw_df["return"].rolling(f_lookback).apply(feature_map[f_funcstr])
+                )
+
+            selected_features = ["return", "tcost"] + features
+            data_dict[ticker] = raw_df[selected_features]
+
+        return data_dict
 
 # =============================================================================
 # Exceptions
@@ -234,6 +353,8 @@ class Data_Loader_CSV(Data_Loader):
 class TimeInvalid(Exception):
     pass
 
+class EmptyDatabase(Exception):
+    pass
 
 # =============================================================================
 # Test
@@ -255,3 +376,21 @@ if __name__ == "__main__":
         ["return_5", "volatility_20", "skewness_20", "kurtosis_20"]
     )
     print(features)
+    
+    
+    #Test MongoDB Loader
+    # data_loader_mongo = Data_Loader_mongo(
+    #     "kaggle_db",
+    #     ["DIS", "GE", "AAPL"],
+    #     [],
+    #     datetime(2016, 9, 23),
+    #     datetime(2016, 11, 7),
+    # )
+    # data_mongo = data_loader_mongo.load_data()
+    
+    #NOTE: MongoDB loader is complete, 
+    #I am getting still working on getting the csv to mongodb loader. (Basically done, just need to make sure format)
+    #DO NOT run the mongoDB loader without the data loaded into mongoDB.
+    #Apologies for the delay
+    
+    pass
