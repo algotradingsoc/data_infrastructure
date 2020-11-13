508b06afd438b9520448d10796812be2d9cca71b
Kevin Xu <58110712+kevinxuht@users.noreply.github.com>
2020-11-13 16:56:49 +0000

    Create getDates.py
    write csv of trading dates from unzipped data

---
+++ getDates.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import os
+import pandas as pd
+
+path = 'c:\\Users\\xu_ho\\Downloads\\Kaggle US EOD 1994_2019\\data'
+
+arr = os.listdir(path)
+
+df = pd.DataFrame(arr, columns = ['raw_name'])
+
+df['date'] = df['raw_name'].map(lambda x: x.split('.')[0])
+
+df['date'] = pd.to_datetime(df['date'])
+
+df.drop('raw_name', axis = 1)
+
+df.to_csv('trading_dates.csv')
+
